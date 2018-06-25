473a0867e0d8e14a8fadabf41027d9802d6c5f91
EC2 Default User <ec2-user@ip-172-31-22-229.us-east-2.compute.internal>
2018-06-25 02:46:01 +0000

    update

--- apps/customer_tag.rb
+++ apps/customer_tag.rb
@@ -12,11 +12,18 @@ class TagCustomers
   def initialize
     @shop_url = "https://#{ENV["SHOPIFY_API_KEY"]}:#{ENV["SHOPIFY_PASSWORD"]}@#{ENV["SHOP"]}.myshopify.com/admin"
     ShopifyAPI::Base.site = @shop_url
+    # puts @shop_url
   end
+  
+  # Tests the Shopify connection with a simple GET request
+  def test_connection
+    return ShopifyAPI::Shop.current
+  end
+  
   
 end
  
 # Called when the file is run on the command line, but not in a require
 if __FILE__ == $PROGRAM_NAME
-  TagCustomers.new
+  puts TagCustomers.new.test_connection.inspect
 end
