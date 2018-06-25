afb0c0cd4e177dbc01ec750e5b84103e82a63939
EC2 Default User <ec2-user@ip-172-31-22-229.us-east-2.compute.internal>
2018-06-25 02:32:26 +0000

    update

--- apps/customer_tag.rb
+++ apps/customer_tag.rb
@@ -5,6 +5,15 @@ require 'dotenv'
 Dotenv.load
 
 class TagCustomers
+    
+  attr_accessor :shop_url
+ 
+  # Configure the Shopify connection
+  def initialize
+    @shop_url = "https://#{ENV["SHOPIFY_API_KEY"]}:#{ENV["SHOPIFY_PASSWORD"]}@#{ENV["SHOP"]}.myshopify.com/admin"
+    ShopifyAPI::Base.site = @shop_url
+  end
+  
 end
  
 # Called when the file is run on the command line, but not in a require
