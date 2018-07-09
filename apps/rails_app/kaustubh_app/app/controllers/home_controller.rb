aaa4d5c920f30ff4000febddb1d86d6167e4f1cf
Kaustubh Singh <kaustubh.singh@gmail.com>
2018-07-09 02:27:47 +0000

    update

--- apps/rails_app/kaustubh_app/app/controllers/home_controller.rb
+++ apps/rails_app/kaustubh_app/app/controllers/home_controller.rb
@@ -2,17 +2,22 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    @customers = ShopifyAPI::Customer.find(:all)
-    
+
   end
   
-  def getCustomerEmails
+  def getEmails
+
+    @customers = ShopifyAPI::Customer.find(:all)
+    
     @emails = []
   
     for a in @customers
       @emails << a.email
     end
     
-  end
+    render "test"
   
+  end
+
+
 end
