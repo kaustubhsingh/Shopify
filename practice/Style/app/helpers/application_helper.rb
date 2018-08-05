271aff692822942ec0adf3c26a61ec73ab3e58c5
Kaustubh Singh <kaustubh.singh@gmail.com>
2018-08-04 22:48:40 -0400

    update

--- practice/Style/app/helpers/application_helper.rb
+++ practice/Style/app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
 
-	def need_stylesheet(name_string)
-		return "<link href=#{name_string} rel=\"stylesheet\">".html_safe
+	
+	def need_stylesheet(file_name)
+		@css_flag = []
+		if not @css_flag.include? file_name 
+			@css_flag << file_name
+			stylesheet_link_tag file_name
+		end
+		
 	end
 	
 end
