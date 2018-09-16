af792ceb688a55c49233043b7c13881c885f4236
Kaustubh Singh <kaustubh.singh@gmail.com>
2018-09-16 11:15:28 -0400

    awesome updates

---
+++ practice/recipe.rb
@@ -0,0 +1,15 @@
+=begin
+reference pages:
+
+1. https://github.com/jhu-ep-coursera/fullstack-course1-module3/blob/master/Assignments/Lesson03-Assignment01-HTTP-Client/README.pdf
+2. https://github.com/zfz/rails-specialization/tree/master/graded-assignments/course01/module03/assignment01-Recipe-Hunter
+
+=end
+
+class Recipe
+
+	api_key = ENV['FOOD2FORK_KEY']
+
+	base_uri "food2fork.com/api/search"
+
+end
